3849272c54324db3bf3a18e563ef98379ef14bdc
Eugene Triguba <eugenetriguba@gmail.com>
2023-01-07 13:37:27 -0500

    Add zsh config

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -7,34 +7,47 @@ in
     programs = {
         direnv = {
             enable = true;
-	    enableZshIntegration = true;
-	};
-	zsh = {
-	    enable = true;
-	    shellAliases = {
-	        # Git
-		gs = "git status";
-		gc = "git commit";
-		ga = "git add";
-		gd = "git diff";
-		gds = "git diff --staged";
-		gp = "git push";
-		gpo = "git push origin";
-		gpl = "git pull";
-		gl = "git log";
-		gch = "git checkout";
-		gv = "git log --graph --decorate --oneline";
+            enableZshIntegration = true;
+        };
+        zsh = {
+            enable = true;
+            enableCompletion = true;
+            autosuggestions.enable = true;
+            syntaxHighlighting.enable = true;
+            ohMyZsh = {
+                enable = true;
+                plugins = [
+                    "gitfast"
+                    "history"
+                    "sudo"
+                    "docker"
+                ];
+                theme = "simple";
+            };
+            shellAliases = {
+                # Git
+                gs = "git status";
+                gc = "git commit";
+                ga = "git add";
+                gd = "git diff";
+                gds = "git diff --staged";
+                gp = "git push";
+                gpo = "git push origin";
+                gpl = "git pull";
+                gl = "git log";
+                gch = "git checkout";
+                gv = "git log --graph --decorate --oneline";
 
-		# General
-		h = "history 25";
-		j = "jobs -l";
-		l = "ls -al";
-		o = "xdg-open .";
-		open = "xdg-open";
+                # General
+                h = "history 25";
+                j = "jobs -l";
+                l = "ls -al";
+                o = "xdg-open .";
+                open = "xdg-open";
 
-		vi = "nvim";
-		vim = "nvim";
-	    };
-	};
+                vi = "nvim";
+                vim = "nvim";
+            };
+        };
     };
 }
