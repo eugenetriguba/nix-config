a33d0451f2e1547ae24f2a2a12cf1a2486086e5a
Eugene Triguba <eugenetriguba@gmail.com>
2023-01-10 17:13:20 -0500

    Remove zsh powerlevel10k theme

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -14,24 +14,24 @@ in
             enableCompletion = true;
             enableAutosuggestions = true;
             enableSyntaxHighlighting = true;
-	    plugins = [
-	        {
-		    name = "powerlevel10k";
-		    src = pkgs.zsh-powerlevel10k;
-		    file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-		}
-		{
-		    name = "powerlevel10k-config";
-		    src = lib.cleanSource ./p10k-config;
-		    file = "p10k.zsh";
-		}
-	    ];
+            plugins = [
+                {
+                    name = "powerlevel10k";
+                    src = pkgs.zsh-powerlevel10k;
+                    file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+                }
+                {
+                    name = "powerlevel10k-config";
+                    src = lib.cleanSource ./p10k-config;
+                    file = "p10k.zsh";
+                }
+            ];
             oh-my-zsh = {
                 enable = true;
                 plugins = [
                     "git"
                 ];
-                theme = "powerlevel10k";
+                # theme = "powerlevel10k";
             };
             shellAliases = {
                 # Git
